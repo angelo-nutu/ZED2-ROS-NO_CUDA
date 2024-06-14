0e226c11a7da17b7cd2c29e535112e22bf52dff0
angelo-nutu <nutuangelo@yahoo.it>
2024-06-14 05:34:21 +0000

    added first working image capture

---
+++ src/zed2_node.cpp
@@ -0,0 +1,130 @@
+#include <videocapture.hpp>
+#include <sensorcapture.hpp>
+#include <ros/ros.h>
+#include <sensor_msgs/Image.h>
+#include <sensor_msgs/Imu.h>
+#include <cv_bridge/cv_bridge.h>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+#define FPS sl_oc::video::FPS::FPS_60
+#define RES sl_oc::video::RESOLUTION::HD720
+#define VERBOSITY sl_oc::VERBOSITY::INFO
+
+class ZED2Node {
+public:
+    ZED2Node() : nh("~"), sensors(VERBOSITY) {
+        initCapture();
+
+        img_pub1 = nh.advertise<sensor_msgs::Image>("/camera1/img", 10);
+        img_pub2 = nh.advertise<sensor_msgs::Image>("/camera2/img", 10);
+        imu_pub = nh.advertise<sensor_msgs::Imu>("/imu/data", 10);
+
+        img_rate = nh.param<double>("img_publish_rate", 60.0);
+        imu_rate = nh.param<double>("imu_publish_rate", 400.0);
+
+        img_timer = nh.createTimer(ros::Duration(1.0 / img_rate), &ZED2Node::imagePub, this);
+        imu_timer = nh.createTimer(ros::Duration(1.0 / imu_rate), &ZED2Node::imuPub, this);
+    }
+
+    void imagePub(const ros::TimerEvent&) {
+        sl_oc::video::Frame frame = cameras.getLastFrame();
+
+        if (frame.data == nullptr) {
+            ROS_WARN("Dropped a frame!");
+            frame = last_frame;
+        } else {
+            last_frame = frame;
+        }
+
+        cv::Mat frameYUV = cv::Mat(frame.height, frame.width, CV_8UC2, frame.data);
+        cv::Mat frameBGR;
+        cv::cvtColor(frameYUV, frameBGR, cv::COLOR_YUV2BGR_YUYV);
+
+        int width = frame.width;
+        int height = frame.height;
+        int midpoint = width / 2;
+
+        cv::Rect leftROI(0, 0, midpoint, height);
+        cv::Rect rightROI(midpoint, 0, width - midpoint, height);
+
+        cv::Mat left = frameBGR(leftROI);
+        cv::Mat right = frameBGR(rightROI);
+
+        sensor_msgs::ImagePtr img_msg1 = cv_bridge::CvImage(std_msgs::Header(), "bgr8", left).toImageMsg();
+        sensor_msgs::ImagePtr img_msg2 = cv_bridge::CvImage(std_msgs::Header(), "bgr8", right).toImageMsg();
+
+        img_pub1.publish(img_msg1);
+        img_pub2.publish(img_msg2);
+    }
+
+    void imuPub(const ros::TimerEvent&) {
+        const sl_oc::sensors::data::Imu imu = sensors.getLastIMUData(1500);
+        if(imu.valid == sl_oc::sensors::data::Imu::NEW_VAL){
+            
+        }
+
+        sensor_msgs::Imu imu_msg;
+
+        imu_msg.orientation.x = 0.0;
+        imu_msg.orientation.y = 0.0;
+        imu_msg.orientation.z = 0.0;
+        imu_msg.orientation.w = 1.0;
+
+        imu_msg.linear_acceleration.x = 0.0;
+        imu_msg.linear_acceleration.y = 0.0;
+        imu_msg.linear_acceleration.z = 0.0;
+
+        imu_msg.angular_velocity.x = 0.0;
+        imu_msg.angular_velocity.y = 0.0;
+        imu_msg.angular_velocity.z = 0.0;
+
+        imu_pub.publish(imu_msg);
+    }
+
+    void initCapture() {
+        params.res = RES;
+        params.fps = FPS;
+        cameras = sl_oc::video::VideoCapture(params);
+
+        std::vector<int> devices = sensors.getDeviceList();
+        if(devices.size() == 0){
+            ROS_FATAL("No available Zed Camera!");
+            ros::shutdown();
+            exit(EXIT_FAILURE);
+        }
+        if(!sensors.initializeSensors(devices[0])){
+            ROS_FATAL("Can't connect to the device!");
+            ros::shutdown();
+            exit(EXIT_FAILURE);
+        }
+        if (!cameras.initializeVideo()) {
+            ROS_FATAL("Cannot open camera video capture!");
+            ros::shutdown();
+            exit(EXIT_FAILURE);
+        }
+
+        std::cout << "Connected to camera sn: " << cameras.getSerialNumber() << "[" << cameras.getDeviceName() << "]" << std::endl;
+    }
+
+private:
+    sl_oc::video::VideoParams params;
+    sl_oc::video::VideoCapture cameras;
+    sl_oc::sensors::SensorCapture sensors;
+    sl_oc::video::Frame last_frame;
+    ros::NodeHandle nh;
+    ros::Publisher img_pub1;
+    ros::Publisher img_pub2;
+    ros::Publisher imu_pub;
+    ros::Timer img_timer;
+    ros::Timer imu_timer;
+    double img_rate;
+    double imu_rate;
+};
+
+int main(int argc, char** argv) {
+    ros::init(argc, argv, "zed2_node");
+    ZED2Node node;
+    ros::spin();
+    return 0;
+}
